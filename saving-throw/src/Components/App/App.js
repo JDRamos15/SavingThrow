ff42e3d09c601804d73ea5c75ae5e5d6bbbf4e8a
Angel D. Burgos <angel.burgos11@upr.edu>
2021-02-27 15:41:26 -0400

    Starting

---
+++ saving-throw/src/Components/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import Home from "../Home/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/*<Route exact path="/createaccount" component={CreateAccount} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/profile:email" component={Profike} />*/}
+        {/* <Route exact path="/home" component={Home} /> */}
+        {/* In case no route does not exist, can create a 404 page341 */}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
