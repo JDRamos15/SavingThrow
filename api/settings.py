db644fb006df0d10a9b8f1f1d1e7adf8a5d5d233
Angel D. Burgos <angel.burgos11@upr.edu>
2021-04-03 16:44:46 -0400

    testing login

--- api/settings.py
+++ api/settings.py
@@ -3,6 +3,6 @@ import os
 # APP_SETTINGS = os.environ.get('APP_SETTINGS')
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
