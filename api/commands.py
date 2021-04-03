e7b8aee58dce123ef73a850fd3d57907eb59f7de
JDRamos15 <juan.ramos15@upr.edu>
2021-04-02 20:14:27 -0400

    Still working on the deployment

--- api/commands.py
+++ api/commands.py
@@ -1,9 +1,9 @@
 import click
 from flask.cli import with_appcontext
 
-from .extension import db
-from .Models.User import userModel
-from .Models.Campaign import campaignModel
+from extension import db
+from Models.User import userModel
+from Models.Campaign import campaignModel
 
 @click.command(name='create_tables')
 @with_appcontext
