adb18e2dcf8031ccfd303529fd1db0ff99e55d02
JDRamos15 <juan.ramos15@upr.edu>
2021-05-02 16:17:32 -0400

    minor changes

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,9 +1,11 @@
-import {useRef, useState} from "react";
-import {useForm} from "react-hook-form";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 import { isPropertySignature } from "typescript";
-import {login} from "../../Services/authentication";
+import { login } from "../../Services/authentication";
 import { useHistory } from "react-router-dom";
-import {isLogged,getPublicId,getUsername, logout} from "../../Services/authentication";
+import { isLogged, getPublicId, getUsername, logout } from "../../Services/authentication";
+import { Card } from "react-bootstrap";
+import './Login.css';
 
 
 
@@ -12,85 +14,84 @@ interface FormData {
     password: string;
 
 }
-export default function Login(props: { history: string[]; }){
-    const {register, handleSubmit, errors,} = useForm<FormData>({
-        defaultValues:{
-            username: "bobyWillRockU",
-            password: "R0b#rt123",
-        }
-    });
+export default function Login(props: { history: string[]; }) {
+    const { register, handleSubmit, errors, } = useForm<FormData>({});
     const [submitting, setSubmitting] = useState<boolean>(false);
     const [serverErrors, setServerErrors] = useState<Array<string>>([]);
-    return <form onSubmit={handleSubmit(async(formData)=>{
-            setSubmitting(true);
-            setServerErrors([]);
-            console.log(formData, "formData");
-            
-            const response = await fetch("/api/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username: formData.username,
-                    password: formData.password,
-                })
-            });
-            const data = await response.json();
-            if (data['status'] == "Success"){
-                console.log(data)
-                console.log(data['username'])
-                login(data['loggedIn'], data['token'], data['username'], data['public_id'], data['fname'])
-                // props.history.push('/profile/'+data['username']);
-                window.location.href='/profile/'+data['username']
-            }
-            else{
-                setServerErrors([data['error']]);
-            }
-            // if (data[1] != 200){
-            //     console.log("Login Unsuccesful")
-            //     return;
-            // }
-            // else{
-            //     props.history.push('/profile');
-            // }
-            
+    return (
+        <div style= {{height: '100%'}}>
+            <Card bg='info' className="outerCard">
+                <Card className='innerCard'>
+                    <Card.Body>
+                        <form onSubmit={handleSubmit(async (formData) => {
+                            setSubmitting(true);
+                            setServerErrors([]);
 
-            setSubmitting(false);
-        })}>
-            {serverErrors ? (
-            <ul>
-                {serverErrors.map((error) => ( 
-                <li key={error}>{error}</li>
-                ))}
-            </ul>
-           ): null }
+                            const response = await fetch("/api/login", {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({
+                                    username: formData.username,
+                                    password: formData.password,
+                                })
+                            });
+                            const data = await response.json();
+                            if (data['status'] == "Success") {
+                                login(data['loggedIn'], data['token'], data['username'], data['public_id'], data['fname'])
+                                window.location.href = '/profile/' + data['username']
+                            }
+                            else {
+                                setServerErrors([data['error']]);
+                            }
 
-        <div>
-            <label htmlFor="username"> Username</label>
-            <input 
-                type="username" 
-                name="username" 
-                id="username" 
-                ref={register({required: {
-                    value: true,
-                    message: "Please enter valid username."
-                }})} 
-            />
-             {errors.username ? <div>{errors.username.message} </div>: null}
+
+                            setSubmitting(false);
+                        })}>
+                            {serverErrors ? (
+                                <ul>
+                                    {serverErrors.map((error) => (
+                                        <li key={error}>{error}</li>
+                                    ))}
+                                </ul>
+                            ) : null}
+
+                            <div>
+                                <h1>Login</h1>
+                                <label htmlFor="username"> Username</label>
+                                <input
+                                    type="username"
+                                    name="username"
+                                    id="username"
+                                    ref={register({
+                                        required: {
+                                            value: true,
+                                            message: "Please enter valid username."
+                                        }
+                                    })}
+                                />
+                                {errors.username ? <div>{errors.username.message} </div> : null}
+                            </div>
+                            <div>
+                                <label htmlFor="password"> Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    ref={register({ required: "required" })}
+                                />
+                                {errors.password ? <div>{errors.password.message} </div> : null}
+                            </div>
+                            <div>
+                                <button type="submit" disabled={submitting}> Login </button>
+                            </div>
+                        </form>
+                    </Card.Body>
+                </Card>
+            </Card>
+
         </div>
-        <div>
-            <label htmlFor="password"> Password</label>
-            <input 
-                type="password" 
-                name="password" 
-                id="password" 
-                ref={register({required: "required"})} 
-            />
-             {errors.password ? <div>{errors.password.message} </div>: null}
-        </div>
-        <div>
-            <button type="submit" disabled = {submitting}> Login </button>
-        </div>
-    </form>;
+
+    );
 }
